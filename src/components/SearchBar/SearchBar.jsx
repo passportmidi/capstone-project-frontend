3fdeb7eb1bef6a34c27416a2153bbb5ed6e6fa30
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-13 13:11:10 -0300

    add state to search bar, will connect to API later in project

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,23 @@
-import searchIcon from "../../assets/images/ic--baseline-search.svg";
 import "./SearchBar.scss";
+import { useState } from "react";
 
 export default function SearchBar() {
+  const [input, setInput] = useState("");
+
+  const handleChange = (value) => {
+    setInput(value);
+    // console.log(input);
+  };
   return (
     <div className="search__wrapper">
-      <label className="search__label">Ingredient:
-      <input placeholder="Search for ingredient..."/>
+      <label className="search__label">
+        Ingredient:
+        <input
+          className="search__input"
+          placeholder="Search for ingredient..."
+          value={input}
+          onChange={(e) => handleChange(e.target.value)}
+        />
       </label>
     </div>
   );
