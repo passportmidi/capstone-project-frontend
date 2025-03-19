a68d1be4c70a14e07eabdca6a987b7741ec09c0c
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-18 22:57:09 -0300

    map ingredients to table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,7 +1,30 @@
 import editIcon from "../../assets/images/ic--baseline-edit.svg";
 import deleteIcon from "../../assets/images/ic--baseline-delete.svg";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export default function Table() {
+  const [ingredients, setIngredients] = useState(null);
+
+  useEffect(() => {
+    fetchIngredients();
+  }, []);
+
+  async function fetchIngredients() {
+    try {
+      const { data } = await axios.get(`${BASE_URL}/ingredients`);
+      setIngredients(data);
+    } catch (e) {
+      console.log("Error fetching ingredients:", e);
+    }
+  }
+
+  if (!ingredients) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <table>
       <thead>
@@ -28,6 +51,23 @@ export default function Table() {
           <td>1 cup</td>
           <td>150g</td>
         </tr>
+        {ingredients.map((ingredient) => {
+          return (
+            <tr key={ingredient.id}>
+              <td>
+                {ingredient.name}
+                {ingredient.custom === 1 && (
+                  <>
+                    <img src={editIcon} alt="Edit" />
+                    <img src={deleteIcon} alt="Delete" />
+                  </>
+                )}
+              </td>
+              <td>{ingredient.volume}</td>
+              <td>{ingredient.grams}</td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
