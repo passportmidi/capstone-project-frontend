d98ba06b3066f2af66530770db79977134619d2b
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-19 21:33:11 -0300

    add title and body fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.scss";
 import Table from "./components/Table/Table";
 import SearchBar from "./components/SearchBar/SearchBar";
 import AmountInput from "./components/AmountInput/AmountInput";
@@ -6,7 +7,7 @@ import Toggle from "./components/Toggle/Toggle";
 function App() {
   return (
     <>
-      <h1>Ingredient Weight Calculator</h1>
+      <h1 className="app__title">RecipeWizard</h1>
       <SearchBar />
       <AmountInput />
       <Toggle />
